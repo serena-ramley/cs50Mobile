5142bf15e9476a3e6c81a8e936ac37bdc02ca805
serena-ramley <sramley@gmail.com>
2019-12-18 10:41:15 -0800

    Update 3a-objectMutation.js
    Modified for console log readability. Must use comma and not + to log a object with a description

--- 3a-objectMutation.js
+++ 3a-objectMutation.js
@@ -9,14 +9,18 @@ const o = {
 // Store by reference
 const o2 = o
 
+// Store by value
 const o3 = Object.assign({}, o)
 
 o.a = 'new Value'
 
 o.obj.key = 'another new value'
 
-console.log(o.obj.key)
+// o1:  { a: 'new Value', b: 'b', obj: { key: 'another new value' } }
+console.log("o1: ", o)
 
-console.log(o2)
+// o2:  { a: 'new Value', b: 'b', obj: { key: 'another new value' } }
+console.log("o2: ", o2)
 
-console.log(o3)
+// o3:  { a: 'a', b: 'b', obj: { key: 'another new value' } }
+console.log("o3: ", o3)
