bd2fa9769124fd67b5b0bbc2334a1b57dd06abc9
serena-ramley <sramley@gmail.com>
2019-12-30 15:24:37 -0800

    Added extends example with set

---
+++ L3-React-Classes-Props/2-set.js
@@ -0,0 +1,112 @@
+// Can extend a class to add additional methods or parameters
+// While keeping the original methods available
+
+class MySet extends Set {
+    constructor(arr) {
+        super(arr)
+        this.originalArray = arr
+    }
+
+    add(val) {
+        super.add(val)
+        console.log('added ${val} to the set!')
+    }
+
+    toArray() {
+        return Array.from(this)
+    }
+
+    reset() {
+        return new MySet(this.originalArray)
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{}
